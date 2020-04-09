3a0fc367e1c79fb1eedf47f0b1d0e5bf37aee4de
Angela V <am.vargasg@uniandes.edu.co>
2020-04-08 19:33:01 -0500

    Taller 1 Typescript

---
+++ tallerTypescript2/dataCourses.ts
@@ -0,0 +1,11 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en equipos", "Jose Joaquin Bocanegra", 3),
+  new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+  new Course("TI en las organizaciones", "Jose Orlando Cuevas", 3),
+  new Course("Computación científica para IELE", "Nestor Peña", 3),
+  new Course("Teoría Electromagnética", "Roberto Bustamante", 3), 
+  new Course("Análisis de decisión de inversión", "Laura Vanegas", 3)
+]
